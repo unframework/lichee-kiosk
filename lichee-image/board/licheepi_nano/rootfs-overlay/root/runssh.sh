7e2eaa847d5936c51825cd26a747ecd17e07d7a1
Nick Matantsev <nick.matantsev@gmail.com>
2023-12-18 19:49:59 -0500

    Clean up SSH autodialer script

--- lichee-image/board/licheepi_nano/rootfs-overlay/root/runssh.sh
+++ lichee-image/board/licheepi_nano/rootfs-overlay/root/runssh.sh
@@ -1,14 +1,7 @@
 #!/bin/sh -e
 
-# example Busybox inittab line
-# /dev/ttyS0::respawn:/sbin/getty -L  /dev/ttyS0 115200 vt100 # GENERIC_SERIAL
-
-# example command
-# /sbin/getty -L -n -l /root/runssh.sh /dev/tty1 115200 linux
-
 # small delay for initial WiFi init
-sleep 2
-exec ssh -o "StrictHostKeyChecking=no" -o "IdentitiesOnly=yes" -t -l hello -p 10697 4.tcp.ngrok.io
+sleep 5
 
-echo Waiting before restart...
-sleep 15
+# start SSH client with TTY forwarding
+exec ssh -o "StrictHostKeyChecking=no" -o "IdentitiesOnly=yes" -t -l hello -p 12345 example.com
