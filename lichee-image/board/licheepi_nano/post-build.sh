c66dbdb8477a50960cd673e05689816ec50dbced
Nick Matantsev <nick.matantsev@gmail.com>
2024-02-29 22:40:07 -0500

    Update Dockerfiles for improved caching

--- lichee-image/board/licheepi_nano/post-build.sh
+++ lichee-image/board/licheepi_nano/post-build.sh
@@ -3,4 +3,6 @@ mkdir -p "$1/usr/lib/dhcpcd/dhcpcd-hooks/"
 ln -s /usr/share/dhcpcd/hooks/10-wpa_supplicant "$1/lib/dhcpcd/dhcpcd-hooks/"
 
 # restrict root SSH directory permissions to comply with OpenSSH requirements
+mkdir -p "$1/root/.ssh/id_rsa"
+cp /root/licheepi-nano/root_id_rsa "$1/root/.ssh/id_rsa"
 chmod -R go-rwx "$1/root/.ssh"
